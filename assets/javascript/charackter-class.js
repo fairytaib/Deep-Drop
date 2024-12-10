704ef2d204e40eb23d0844ca6d55218ace11e54b
Sophjan Moubarrid <fairytaib@gmail.com>
2024-12-10 12:03:12 +0000

    Create basic Character Structure

---
+++ assets/javascript/charackter-class.js
@@ -0,0 +1,33 @@
+// Basis Character Class for Monster and Player
+class Character {
+    constructor(health, damage, attackSpeed, defense, dodgeChance) {
+        this.health = health
+        this.damage = damage
+        this.attackSpeed = attackSpeed
+        this.defense = defense
+        this.dodgeChance = dodgeChance
+    }
+
+    attack() {
+        console.log("attack")
+    }
+}
+
+// Player Classes
+// Refine Stats later on
+let knight = new Character(25, 25, 1, 25, 1)
+let ranger = new Character(25, 25, 1, 25, 1)
+let assassin = new Character(25, 25, 1, 25, 1)
+
+// Monster Classes
+// Refine Stats later on
+let slime = new Character(25, 25, 1, 25, 1)
+let swarmOfRats = new Character(25, 25, 1, 25, 1)
+let kobold = new Character(25, 25, 1, 25, 1)
+let goblin = new Character(25, 25, 1, 25, 1)
+let skeletonWarrior = new Character(25, 25, 1, 25, 1)
+let orc = new Character(25, 25, 1, 25, 1)
+let harpie = new Character(25, 25, 1, 25, 1)
+let troll = new Character(25, 25, 1, 25, 1)
+let fireElemental = new Character(25, 25, 1, 25, 1)
+let boss = new Character(25, 25, 1, 25, 1)
