708492d427d1ad37902bb75a47c284374415e02e
Sophjan Moubarrid <fairytaib@gmail.com>
2024-12-10 09:38:45 +0000

    Edit Name-Input-logic

--- assets/javascript/dom-manipulation.js
+++ assets/javascript/dom-manipulation.js
@@ -4,8 +4,12 @@ function validateInput() {
     const input = document.getElementById("player-name-input").value
     const pattern = /^[a-zA-Z0-9]+$/;
 
-    if (pattern.test(input)) {
-        console.log("Wrong")
+    if (input.length <= 2) {
+        alert("To short")
+    } else if (!pattern.test(input)) {
+        alert("Wrong")
+    } else if (input.length > 20) {
+        alert("To long")
     } else {
         playerName = input
     }
